b9730e9444180176bd82573931caa8955a919bab
JFronny <git@akbvopenfl1.hopto.org>
2026-05-09 21:43:03 +0200

    fix: use FloatingActionButtonMenu

--- composeApp/src/commonMain/kotlin/dev/jfronny/zerointerest/ui/RoomScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/jfronny/zerointerest/ui/RoomScreen.kt
@@ -15,23 +15,31 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.CompareArrows
+import androidx.compose.material.icons.automirrored.filled.NoteAdd
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Paid
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonMenu
+import androidx.compose.material3.FloatingActionButtonMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleFloatingActionButton
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteItem
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -184,23 +192,37 @@ fun RoomScreen(
                 } else {
                     var expanded by remember { mutableStateOf(false) }
                     Box {
-                        FloatingActionButton(onClick = { expanded = true }) {
-                            Icon(Icons.Default.Add, stringResource(Res.string.add_transaction))
-                        }
-                        DropdownMenu(
+                        FloatingActionButtonMenu(
                             expanded = expanded,
-                            onDismissRequest = { expanded = false }
+                            button = {
+                                TooltipBox(
+                                    positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                                        if (expanded) TooltipAnchorPosition.Start else TooltipAnchorPosition.Above,
+                                    ),
+                                    tooltip = { PlainTooltip { Text(stringResource(Res.string.new_transaction)) } },
+                                    state = rememberTooltipState()
+                                ) {
+                                    ToggleFloatingActionButton(
+                                        checked = expanded,
+                                        onCheckedChange = { expanded = it },
+                                    ) {
+                                        Icon(Icons.Default.Add, stringResource(Res.string.add_transaction))
+                                    }
+                                }
+                            }
                         ) {
-                             DropdownMenuItem(
+                            FloatingActionButtonMenuItem(
                                 text = { Text(stringResource(Res.string.new_transaction)) },
+                                icon = { Icon(Icons.Default.Add, stringResource(Res.string.new_transaction)) },
                                 onClick = {
                                     expanded = false
                                     onAddTransaction(null)
                                 }
                             )
                             templates.forEach { template ->
-                                DropdownMenuItem(
+                                FloatingActionButtonMenuItem(
                                     text = { Text(template.description) },
+                                    icon = { Icon(Icons.AutoMirrored.Filled.NoteAdd, stringResource(Res.string.add_transaction)) },
                                     onClick = {
                                         expanded = false
                                         onAddTransaction(template)
