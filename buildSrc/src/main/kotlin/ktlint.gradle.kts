7f20b014b4ead447dda196c579b0307ea8e5c5a4
JFronny <git@akbvopenfl1.hopto.org>
2026-09-24 16:52:43 +0200

    build: avoid deprecated API

--- buildSrc/src/main/kotlin/ktlint.gradle.kts
+++ buildSrc/src/main/kotlin/ktlint.gradle.kts
@@ -2,10 +2,10 @@ plugins {
     base
 }
 
-val ktlint by configurations.creating
+val ktlintConfig = configurations.create("ktlint")
 
 dependencies {
-    ktlint(versionCatalogs.named("libs").findLibrary("ktlint").get())
+    ktlintConfig(versionCatalogs.named("libs").findLibrary("ktlint").get())
 }
 
 val outputDir = project.layout.buildDirectory.dir("reports/ktlint/")
@@ -13,22 +13,25 @@ val inputFiles = fileTree("src") { include("**/*.kt") }
 val editorconfig = rootProject.file(".editorconfig").absolutePath
 
 tasks {
-    val ktlintRun by registering(JavaExec::class) {
+    val ktlintRun = register("ktlintRun", JavaExec::class) {
+        description = "Verify that the code is formatted"
         group = "verification"
         inputs.files(inputFiles)
         outputs.dir(outputDir)
         mainClass.set("com.pinterest.ktlint.Main")
-        classpath = ktlint
+        classpath = ktlintConfig
         args = listOf("--editorconfig=$editorconfig", "src/**/*.kt")
         jvmArgs = listOf("--add-opens", "java.base/java.lang=ALL-UNNAMED")
     }
 
-    val ktlintFormat by registering(JavaExec::class) {
+    val ktlintFormat = register("ktlintFormat", JavaExec::class) {
+        description = "Reformat kotlin source code"
         group = "verification"
+        mustRunAfter(ktlintRun) // in CI, we actually want ktlintRun to cause failures with unformatted code
         inputs.files(inputFiles)
         outputs.dir(outputDir)
         mainClass.set("com.pinterest.ktlint.Main")
-        classpath = ktlint
+        classpath = ktlintConfig
         args = listOf("--editorconfig=$editorconfig", "-F", "src/**/*.kt")
         jvmArgs = listOf("--add-opens", "java.base/java.lang=ALL-UNNAMED")
     }
